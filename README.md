3e02f4b29d6e86944d5506cc0c19e21c31b04909
Mikhail <me@reze.moe>
2023-11-01 21:55:46 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# New mishashto website
+
+Completely static website, that implements dynamic pages, through fetch functionality.
+
+## Run locally
+
+```bash
+$ python3 serve_locally.py
+```
+
+### Limitations
+
+1. Pages can not implement JavaScript, you have to add preload-script, implementation, like on <a target="_blank" href="https://uwu.so">uwu.so</a>.
---
+++ profile_info.js
@@ -0,0 +1,35 @@
+const Profile = {
+    profile_picture: "base.png",
+    name: {
+        nickname: "mishashto",
+        actual_name: "Mikhail"
+    },
+    countries: {
+        born: "RU",
+        current: "JP"
+    },
+    description: "I do goofy stuff for a living (not really). Previously a back-end developer, now just a goofball.",
+    born_unix: 1057633200,
+    links: [
+        {
+            name: "GitHub",
+            icon: "",
+            link: "https://github.com/LMNYX"
+        },
+        {
+            name: "YouTube",
+            icon: "",
+            link: "https://www.youtube.com/@mishashto"
+        },
+        {
+            name: "Twitter",
+            icon: "",
+            link: "https://twitter.com/mishashtoes"
+        },
+        {
+            name: "uwu.so",
+            icon: "",
+            link: "https://uwu.so"
+        }
+    ]
+}
